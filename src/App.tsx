6c55da4151822ecebc4602a76fddf69a6f4ca2c0
Nana Adjei Manu <claeusdev@gmail.com>
2021-11-08 00:53:24 +0000

    feat: setup base payroll list display

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
-import axios from "axios";
+import { Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import Api from "src/api"
+import Api from "src/api";
 import Application from "src/components/App";
 
+export interface Payroll {
+  ["Payroll Period"]: string;
+  ["Pay Date"]: string;
+  ["Employee ID"]: string;
+  ["Employee Name"]: string;
+  Departments: string[];
+  currency: string;
+  ["Hours Worked"]: number;
+  ["Base Salary"]: number;
+  Bonus: number;
+  ["Student Loan Repayment"]: number;
+  ["Income Tax"]: number;
+  ["Employee Pension"]: number;
+  ["Employer Pension"]: number;
+  ["Net Pay"]: number;
+}
+
 function App() {
-  const [state, setstate] = useState()
+  const [state, setstate] = useState<Payroll[]>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
   useEffect(() => {
     async function getProduct() {
-      const response = await new Api().getPayroll()
-      console.log("RES", { response })
+      setIsLoading(true);
+      const response = await new Api().getPayroll();
+      setstate(response);
+      setIsLoading(false);
     }
     getProduct();
   }, []);
   return (
     <div className="App">
+      {isLoading ? (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      ) : (
+        <>
+          {state.map((payroll) => (
+            <p>{payroll["Employee Name"]}</p>
+          ))}
+        </>
+      )}
       <Application />
     </div>
   );

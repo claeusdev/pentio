49d1b2406e7381584c9961475d896ced56e950bd
Nana Adjei Manu <claeusdev@gmail.com>
2021-11-08 00:37:08 +0000

    Complete setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Api from "src/api"
 import Application from "src/components/App";
 
 function App() {
+  const [state, setstate] = useState()
+  useEffect(() => {
+    async function getProduct() {
+      const response = await new Api().getPayroll()
+      console.log("RES", { response })
+    }
+    getProduct();
+  }, []);
   return (
     <div className="App">
       <Application />
